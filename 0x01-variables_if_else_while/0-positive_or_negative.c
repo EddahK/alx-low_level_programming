703d0e55faa4625416ca440666bfc9785b84fa0b
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-15 21:17:02 +0300

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*it executes, and prints it
- * Return: Always 0 (Success)*/
- /* main - assigns a random number to int n everytime */
+/*Betty style*/
+ 
+ /* main - assigns a random number to int n everytime it executes, and prints it Return: Always 0 (Success) */
 int main(void)
 {
 	int n;
