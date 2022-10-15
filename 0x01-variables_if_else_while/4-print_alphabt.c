05e0b9664a03528d1c3418843e607b2de0aaaefd
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-15 23:27:03 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,16 @@
  */
 int main(void)
 {
+	char ch = 'a';
 
-  char ch = 'a';
-  
   while (ch <= 'z')
-	{
+  {
       if (ch != 'e' && ch != 'q')
       {
-          putchar(ch); 
+	      putchar(ch);
       }
-      ch++;
-	}
+	  ch++;
+  }
 	putchar('\n');
 return (0);
 }
