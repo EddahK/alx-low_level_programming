e99d65e0ad516672a394e65fbac86e406ff9d687
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-18 20:08:18 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ */
+void print_alphabet_x10(void);
+{
+  char i;
+  int x = 0;
+  
+  while (x <= 9)
+  {
+    for (i = 'a'; i <= 'z'; i++)
+    {
+      _putchar(i);?
+      }
+
+	_putchar('\n');
+  
+   c++;
+  }
+}
