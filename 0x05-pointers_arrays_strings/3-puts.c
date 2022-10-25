adac18cca411d9b08e8f2b1413b9005bec89b49d
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-25 14:42:01 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * _puts - a function that prints a string, followed by a new line
- *
+ * @str: string to be printed
  * Return: 0.
  */
 void _puts(char *str)
 {
-str = " I do not fear computers. I fear the lack of them - - Isaac Asimov";
+str = "I do not fear computers. I fear the lack of them - - Isaac Asimov";
 puts(str);
-putchar('\n');
+puts('\n');
 }
