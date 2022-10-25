3421115d8ad3bef92cf9615dace34f38b0447263
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-25 16:27:26 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,21 +8,21 @@
 */
 void puts_half(char *str)
 {
-  int len = 0;
+int len = 0;
 
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
+while (*str != '\0')
+{
+len++;
+str++;
+}
 
-	str -= (len / 2);
-	while (*str != '\0')
-	{
-		putchar(*str);
-		str++;
-	}
+str -= (len / 2);
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
 
-	putchar('\n');
+putchar('\n');
   
 }
