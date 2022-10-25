8f67eaeca64dd908bf56ae84f72a14ce333e8325
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-25 14:38:52 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line
+ *
+ * Return: 0.
+ */
+void _puts(char *str)
+{
+str = " I do not fear computers. I fear the lack of them - - Isaac Asimov";
+puts(str);
+putchar('\n');
+}
