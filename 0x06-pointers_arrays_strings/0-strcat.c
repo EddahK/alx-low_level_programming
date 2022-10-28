c6bca273b98423a0523d2944c420aa938cd9dd2b
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-28 15:23:36 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,24 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
-}
+	char size_t n;
+	size_t dest_len = strlen(dest);                                         
+               size_t i;                                                               
+                                                                                       
+               for (i = 0 ; i < n && src[i] != '\0' ; i++)                             
+                   dest[dest_len + i] = src[i];                                        
+               dest[dest_len + i] = '\0';                                              
+                                                                                       
+return dest;                                                            
+}               
