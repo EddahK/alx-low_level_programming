1dd8cb9ee848724206f3e90830fde1281d795eb8
EddahK <47106054+EddahK@users.noreply.github.com>
2022-10-28 13:18:38 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,11 +3,11 @@
 /**
  * main - function that prints an integer.
  * @n: integer to print
- * 
+ *
  */
 void print_number(int n)
 {
-  unsigned int num = n;
+unsigned int num = n;
 
 	if (n < 0)
 	{
