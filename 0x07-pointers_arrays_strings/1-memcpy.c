bcafdda720b1bbd7520aaca9095971170b29535d
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-02 16:37:04 +0300

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: area to copy to
+ * @src: area to copy from
+ * @n: bytes to copy
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
