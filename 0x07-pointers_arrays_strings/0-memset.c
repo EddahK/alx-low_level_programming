98fdd3a8d299bbe29f9a3b1d19012b808da2c655
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-02 16:12:31 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte.
+ * @*s: pointer
+ * @b: constant byte to replace n
+ * @n: bytes to be replaced
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+  
+}
