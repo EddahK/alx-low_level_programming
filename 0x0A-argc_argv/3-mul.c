23397b56769238e30ff24edf6fe28e26724b827a
EddahK <wangu.kin@gmail.com>
2022-11-07 04:12:28 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,21 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, j, product;
 
-	if (argc == 1 || argc == 2)
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		j = 1;
-
-		for (i = 1; i < 3; i++)
-			j *= atoi(argv[i]);
-
-		printf("%d\n", j);
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		product = i * j;
+		printf("%d\n", product);
 	}
+
 	return (0);
 }
