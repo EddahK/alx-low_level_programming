25d3b3911387d9f7884f954b636b40d971a159b5
EddahK <wangu.kin@gmail.com>
2022-11-07 02:50:22 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,13 @@
+#include "main.h"
 #include <stdio.h>
-#define UNUSED(X) (void)(x)
-
 /**
   * main - prints program name
   * @argv: the array of argument
   * @argc: The count of arguments
   * Return: 0
   */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
