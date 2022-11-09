2a392908d932e6d336679f338ca07272fdebb038
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-09 15:09:08 +0300

    Create 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+  * argstostr - concatenates all the arguments of your program 
+  * @ac: takes in width of grid
+  * @av: height of grid
+  * Return: a pointer to a new string, or NULL if it fails
+  */
+char *argstostr(int ac, char **av)
+{
