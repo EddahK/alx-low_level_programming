1ec30086a9216f6dddb90f8ce45f261727aba320
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-15 15:17:23 +0300

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * new_dog - function that creates a new dog
+  * @name: dog name
+  * @age: dog age
+  * @owner: owner's name
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  char *temp_name;
+	char *temp_owner;
+	dog_t *new_doggo;
+
+	temp_name = malloc(sizeof(*name) * (strlen(name) + 1));
+	temp_owner = malloc(sizeof(*owner) * (strlen(owner) + 1));
+	new_doggo = malloc(sizeof(dog_t));
+
+	if (temp_name && temp_owner && new_doggo)
+	{
+		strcpy(temp_name, name);
+		strcpy(temp_owner, owner);
+		new_doggo->name = temp_name;
+		new_doggo->age = age;
+		new_doggo->owner = temp_owner;
+	}
+	else
+	{
+		free(temp_name);
+		free(temp_owner);
+		free(new_doggo);
+		return (NULL);
+	}
+	return (new_doggo);
+}
