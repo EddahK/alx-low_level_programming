b5fe0f451bd3240bc1ef7fca440883b15cf23750
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-18 14:40:10 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
-#include "function_pointers.h"                                                                                                                                                             
-                                                                                                                                                                                            
-/**                                                                                                                                                                                         
-  * print_name - function prints name                                                                                                                                                       
-  * @name: name to print                                                                                                                                                                    
-  * @f: pointer to the print function                                                                                                                                                       
-  * Return: name                                                                                                                                                                            
-  */                                                                                                                                                                                        
-void print_name(char *name, void (*f)(char *))                                                                                                                                              
-{                                                                                                                                                                                           
-        if (!name || !f)                                                                                                                                                                    
-                return;                                                                                                                                                                     
-        f(name);
-}                               
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  if (!name || !f)
+		return;
+	f(name);
+}
