e054442a60a6eb5fd6a9569185dfcf42c6351441
EddahK <wangu.kin@gmail.com>
2022-11-18 05:15:53 -0800

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,14 +1,14 @@
 #include "3-calc.h"
 
 /**
-  * op_add - sums up two integers 
+  * op_add - sums up two integers
   * @a: first integer
   * @b: second integer
   * Return: sum of a and b
   */
 int op_add(int a, int b)
 {
-  return (a + b);
+	return (a + b);
 }
 
 /**
@@ -19,7 +19,7 @@ int op_add(int a, int b)
   */
 int op_sub(int a, int b)
 {
-  return (a - b);
+	return (a - b);
 }
 
 /**
@@ -30,7 +30,7 @@ int op_sub(int a, int b)
   */
 int op_mul(int a, int b)
 {
-  return (a * b);
+	return (a * b);
 }
 
 /**
@@ -41,7 +41,7 @@ int op_mul(int a, int b)
   */
 int op_div(int a, int b)
 {
-  return (a / b);
+	return (a / b);
 }
 
 /**
@@ -50,7 +50,7 @@ int op_div(int a, int b)
   * @b: second integer
   * Return: reminder of a % b
   */
- int op_mod(int a, int b)
- {
-  return (a % b);
- }
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
