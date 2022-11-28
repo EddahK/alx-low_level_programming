7b9cc75e3a716ce76c3709ffa9178ad3c599b76f
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-28 15:17:07 +0300

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * print_list - prints all the elements of a list_t list.
+  * @list_t: list
+  * @*h: pointer
+  * Return: the number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+  
+  
+  
+}
